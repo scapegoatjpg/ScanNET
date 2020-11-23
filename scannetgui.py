968943623fe5f0296f957d062697bbab9fa8961d
scapegoatjpg <raullongoria115@gmail.com>
2020-11-23 17:36:41 -0600

    added the content sections for each tab

--- scannetgui.py
+++ scannetgui.py
@@ -7,18 +7,34 @@ class GUI:
         self.root = tk.Tk()
         self.root.title("ScanNET")
         #all widths and heights aren't official, most likely change
-        self.root.minsize (600, 500)
+        self.root.minsize (800, 600)
 
         #the tabs
         my_notebook = ttk.Notebook(self.root)
         my_notebook.pack()
-        devicestab = Frame(my_notebook, width=600, height=500, bg="blue")
-        reportstab = Frame(my_notebook, width=600, height=500, bg="red")
-        devicestab.pack(fill="both", expand=1)
-        reportstab.pack(fill="both", expand=1)
+        devicestab = Frame(my_notebook, width=800, height=600)
+
+        reportstab = Frame(my_notebook, width=800, height=600)
+        devicestab.pack(fill=BOTH, expand=1)
+        reportstab.pack(fill=BOTH, expand=1)
         my_notebook.add(devicestab, text="Devices")
         my_notebook.add(reportstab, text="Reports")
+
+        #contents for devices tab
+        devicesleft = LabelFrame(devicestab, text="Devices found: ", padx=5, pady=5, width=500, height=600)
+        devicesleft.grid(row=0, column=0)
+        devicesright = LabelFrame(devicestab, text="Activity Feed: ", padx=5, pady=5, width=300 , height=600)
+        devicesright.grid(row=0, column=1)
+
+        #contents for reports tab
+        reportsleft = LabelFrame(reportstab, text="Report Summaries: ", padx=5, pady=5, width=400 , height=600)
+        reportsleft.grid(row=0, column=0)
+        reportsright= LabelFrame(reportstab, text="Charts and Diagrams: ", padx=5, pady=5, width=400 , height=600)
+        reportsright.grid(row=0, column=1)
+
         
+
+
         
     def start(self):
         self.root.mainloop()
