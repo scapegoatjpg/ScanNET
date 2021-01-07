d802070161a54034b7796dc99c59601f7ec37a5e
scapegoatjpg <raullongoria115@gmail.com>
2021-01-06 22:53:03 -0600

    using who_is_on_my_wifi, need to figure out how to find hostnames

--- network_scanner.py
+++ network_scanner.py
@@ -1,5 +1,9 @@
-import socket
+import nmap
+import who_is_on_my_wifi
+from who_is_on_my_wifi import *
+#pip3 install who-is-on-my-wifi (required pip install wmi AND MANUALY INSTALL NMAP [nmap.org/download.html] do nmap-7.91-setup.exe)
 
-hostname = socket.gethostname()
-ipaddress = socket.gethostbyname(hostname)
-print(socket.gethostbyaddr(ipaddress))
+WHO = who()
+for i in range(0, len(WHO)):
+    print(WHO[i])
+    #print(WHO[i][1]) #prints only the ip addresses
