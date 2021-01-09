dfcebf03f439b8782e45a1c73e4a4fa7514be1d2
scapegoatjpg <raullongoria115@gmail.com>
2021-01-09 09:20:40 -0600

    made it to continously scan with a 3 sec delay

--- network_scanner.py
+++ network_scanner.py
@@ -1,9 +1,15 @@
+import time
 import nmap
 import who_is_on_my_wifi
-from who_is_on_my_wifi import *
+from who_is_on_my_wifi import who
 #pip3 install who-is-on-my-wifi (required pip install wmi AND MANUALY INSTALL NMAP [nmap.org/download.html] do nmap-7.91-setup.exe)
 
+ipmacs = {}
 WHO = who()
 for i in range(0, len(WHO)):
-    print(WHO[i])
-    #print(WHO[i][1]) #prints only the ip addresses
+    ipmacs[WHO[i][1]] = WHO[i][3]   #dictionary to put in ip addresses and corresponding MAC addresses
+
+def netting():
+    while True:
+        print(ipmacs)       #prints the list on terminal
+        time.sleep(3)       #delays by 3 seconds
