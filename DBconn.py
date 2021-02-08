7d16306fb7502ab72185deb59eeb9dd606d1eb6b
smurphy9 <shonmurphy@icloud.com>
2021-02-08 17:14:43 -0600

    Updated scripts

--- DBconn.py
+++ DBconn.py
@@ -1,4 +1,42 @@
-def create_db_connection(host_name, user_name, user_password, db_name):
-   
+import mysql.connector #pip install mysql-connector-python-rf
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password,
+            database = db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
     return connection
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connection = create_connection("localhost", "root", "","ScanNet")
+create_database_query = "CREATE DATABASE ScanNet"
+create_database(connection, create_database_query)
