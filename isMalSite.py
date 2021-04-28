792750e37620179309576876ee242459b7ad53ea
scapegoatjpg <raullongoria115@gmail.com>
2021-04-28 08:57:29 -0500

    cleaned up and created checkSite function

--- isMalSite.py
+++ isMalSite.py
@@ -1,14 +1,10 @@
 import pandas as pd
-import numpy as np
-import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 urls_data = pd.read_csv('urldata.csv')
-
 type(urls_data)
-
 urls_data.head()
 
 def makeTokens(f):
@@ -26,20 +22,19 @@ def makeTokens(f):
         total_Tokens.remove('com')
     return total_Tokens
 
-y = urls_data['label']
-url_list = urls_data['url']
+def checkSite(site):
+    y = urls_data['label']
+    url_list = urls_data['url']
 
-vectorizer = TfidfVectorizer(tokenizer=makeTokens)
-X = vectorizer.fit_transform(url_list)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-logit = LogisticRegression()
-logit.fit(X_train, y_train)
+    vectorizer = TfidfVectorizer(tokenizer=makeTokens)
+    X = vectorizer.fit_transform(url_list)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    logit = LogisticRegression()
+    logit.fit(X_train, y_train)
+    logit.score(X_test, y_test)
 
-print('Accuracy: ', logit.score(X_test, y_test))
-X_predict = []
-X_predict.append(input('Enter URL: '))
-
-X_predict = vectorizer.transform(X_predict)
-New_predict = logit.predict(X_predict)
-print(New_predict)
-#currently only prints out if URL input is good or bad, will add functionality to ScanNET
+    X_predict = []
+    X_predict.append(site)
+    X_predict = vectorizer.transform(X_predict)
+    New_predict = logit.predict(X_predict)
+    print('URL: ', site, '\n    Prediction:', New_predict[0])
